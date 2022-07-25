934ff92c4e8d1bda6cc4fb241042f6be008990c8
arthur360cgf <arthurmattei115@gmail.com>
2022-07-25 08:08:39 -0300

    fazendo o thema da pagina inicial

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HangTheme {
+  static const MaterialColor blue = MaterialColor(
+    _hangPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFE3F2FD),
+      100: Color(0xFFBBDEFB),
+      200: Color(0xFF90CAF9),
+      300: Color(0xFF64B5F6),
+      400: Color(0xFF42A5F5),
+      500: Color(_hangPrimaryValue),
+      600: Color(0xFF1E88E5),
+      700: Color(0xFF1976D2),
+      800: Color(0xFF1565C0),
+      900: Color(0xFF0D47A1),
+    },
+  );
+  static const int _hangPrimaryValue = 0xFF2196F3;
+
+  static const Color background = Color(0xFF90CAF9);
+
+  static ButtonStyle outlineButtonStyle({
+    Color color = Colors.white,
+    double padding = 24,
+  }) {
+    return OutlinedButton.styleFrom(
+      primary: color,
+      padding: EdgeInsets.symmetric(vertical: padding),
+      side: BorderSide(color: color),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      ),
+    );
+  }
+
+  static ThemeData theme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: background,
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.grey,
+    textTheme: GoogleFonts.wendyOneTextTheme(
+      ThemeData.dark().textTheme,
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: outlineButtonStyle(),
+    ),
+    appBarTheme: ThemeData.dark().appBarTheme.copyWith(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          titleTextStyle: GoogleFonts.wendyOne(fontSize: 25),
+          centerTitle: true,
+        ),
+  );
+}
