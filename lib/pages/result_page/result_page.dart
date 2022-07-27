c9da007e0a022e11e5fc9fb004f7577d9adc5787
arthur360cgf <arthurmattei115@gmail.com>
2022-07-26 22:30:08 -0300

    fazendo a tela de resultados

--- lib/pages/result_page/result_page.dart
+++ lib/pages/result_page/result_page.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:prod4_aval2_hang_game/theme/theme.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Resultado'),
+    return Card(
+      color: Colors.blueAccent,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                'Results',
+                style: TextStyle(
+                  color: HangTheme.color,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            List
+          ],
+        ),
       ),
     );
   }
